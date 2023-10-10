e249fa8e790104d37952a817209084c99c8ccfd7
Gabriel Pacheco <gabioh2012@gmail.com>
2023-10-09 21:56:51 -0500

    save all

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,6 @@
 import 'react-phone-input-2/lib/style.css'
 import PhoneInput from "react-phone-input-2"
 import { useState } from "react"
-import Link from '@components/ui/link.astro'
 
 const Form = () => {
     const [phoneNumber, setPhoneNumber] = useState(null)
